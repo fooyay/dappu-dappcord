7432a1f1d1f5c363dd7ed695708e511cf31f03ae
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-15 16:57:24 -0500

    add first test

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
 
-contract Dappcord {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Dappcord is ERC721 {
+    // constructor for the Dappcord contract
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,5 +5,21 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
+  describe("Deployment", function () {
+    it("sets the name correctly", async function () {
+      const Dappcord = await ethers.getContractFactory("Dappcord")
+      const dappcord = await Dappcord.deploy("Dappcord", "DCORD")
+      await dappcord.deployed()
+      const result = await dappcord.name()
+      expect(result).to.equal("Dappcord")
+    })
 
+    it("sets the symbol correctly", async function () {
+      const Dappcord = await ethers.getContractFactory("Dappcord")
+      const dappcord = await Dappcord.deploy("Dappcord", "DCORD")
+      await dappcord.deployed()
+      const result = await dappcord.symbol()
+      expect(result).to.equal("DCORD")
+    })
+  })
 })
