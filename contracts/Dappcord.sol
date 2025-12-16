9ce27cdf1eaa1427ab94abce2e74acaa35d7e06f
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-16 16:34:41 -0500

    add withdraw function

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Dappcord is ERC721 {
+    uint256 public totalSupply = 0; // total number of NFTs minted, one for each user/channel joined
     uint256 public totalChannels = 0;
     address public owner;
 
@@ -14,6 +15,7 @@ contract Dappcord is ERC721 {
     }
 
     mapping(uint256 => Channel) public channels; // mapping of channel id to channel details
+    mapping(uint256 => mapping(address => bool)) public hasJoined; // who has joined each channel
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
@@ -30,7 +32,25 @@ contract Dappcord is ERC721 {
         channels[totalChannels] = Channel(totalChannels, _name, _cost);
     }
 
+    function mint(uint256 _channelId) public payable {
+        require(_channelId != 0, "Channel ID cannot be zero");
+        require(_channelId <= totalChannels, "Channel does not exist");
+        require(hasJoined[_channelId][msg.sender] == false, "User has already joined this channel");
+        require(msg.value >= channels[_channelId].cost, "Insufficient funds to join channel");
+        // user is joining channel _channelId
+        hasJoined[_channelId][msg.sender] = true;
+
+        // mint NFT
+        totalSupply++;
+        _safeMint(msg.sender, totalSupply);
+    }
+
     function getChannel(uint256 _id) public view returns (Channel memory) {
         return channels[_id];
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success,) = owner.call{value: address(this).balance}("");
+        require(success, "Withdrawal failed");
     }
 }
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -5,13 +5,13 @@ const tokens = (n) => {
 }
 
 describe("Dappcord", function () {
-  let dappcord, deployer, user
+  let dappcord, deployer, user, user2
 
   const NAME = "Dappcord"
   const SYMBOL = "DCORD"
 
   this.beforeEach(async () => {
-    [deployer, user] = await ethers.getSigners()
+    [deployer, user, user2] = await ethers.getSigners()
 
     // Deploy the Dappcord contract
     const Dappcord = await ethers.getContractFactory("Dappcord")
@@ -51,6 +51,73 @@ describe("Dappcord", function () {
       expect(channel.id).to.equal(1)
       expect(channel.name).to.equal("General")
       expect(channel.cost).to.equal(tokens(1))
+    })
+  })
+
+  describe("Joining Channels", function () {
+    const ID = 1
+    const AMOUNT = tokens(1)
+
+    beforeEach(async () => {
+      const transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+    })
+
+    it('Joins the user', async () => {
+      const result = await dappcord.hasJoined(ID, user.address)
+      expect(result).to.equal(true)
+    })
+
+    it('Increases total supply', async () => {
+      const result = await dappcord.totalSupply()
+      expect(result).to.equal(1)
+    })
+
+    it('Updates contract balance', async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.equal(AMOUNT)
+    })
+
+    it('Reverts if the channel does not exist', async () => {
+      await expect(dappcord.connect(user).mint(2, { value: AMOUNT })).to.be.revertedWith("Channel does not exist")
+    })
+
+    it('Reverts if the payment is insufficient', async () => {
+      await expect(dappcord.connect(user2).mint(ID, { value: tokens(0.5) })).to.be.revertedWith("Insufficient funds to join channel")
+    })  
+
+    it('Reverts if user has already joined', async () => {
+      await expect(dappcord.connect(user).mint(ID, { value: AMOUNT })).to.be.revertedWith("User has already joined this channel")
+    })
+
+    it('Reverts if the channel ID is invalid', async () => {
+      await expect(dappcord.connect(user).mint(0, { value: AMOUNT })).to.be.revertedWith("Channel ID cannot be zero")
+    })
+  })
+
+  describe("Withdrawing Funds", function () {
+    const ID = 1
+    const AMOUNT = tokens(10)
+    let balanceBefore
+
+    beforeEach(async () => {
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      let transaction = await dappcord.connect(user).mint(ID, { value: AMOUNT })
+      await transaction.wait()
+
+      transaction = await dappcord.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+    it('Updates the owner balance', async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address)
+      expect(balanceAfter).to.be.greaterThan(balanceBefore)
+    })
+
+    it('Updates the contract balance', async () => {
+      const result = await ethers.provider.getBalance(dappcord.address)
+      expect(result).to.equal(0)
     })
   })
 })
